87475e188cd7597ac38fd1e21c4fad82e5bbaa59
Andrey Todoshchenko <todoshcenko@gmail.com>
2019-02-11 17:19:03 +0200

    MemoryMetricExporterFactory removed

--- tests/StatsdExporterAwareTraitTest.php
+++ tests/StatsdExporterAwareTraitTest.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace TutuRu\Tests\Metrics;
 
-use TutuRu\Tests\Metrics\MemoryMetricExporter\MemoryMetricExporterFactory;
+use TutuRu\Tests\Metrics\MemoryMetricExporter\MemoryMetricExporter;
 
 class StatsdExporterAwareTraitTest extends BaseTest
 {
     public function testSetMetricsExporter()
     {
-        $statsdExporterClient = MemoryMetricExporterFactory::create($this->config);
+        $statsdExporterClient = new MemoryMetricExporter("unittest");
 
         $object = new StatsdExporterAwareObject();
         $this->assertNull($object->getStatsdExporterClient());
