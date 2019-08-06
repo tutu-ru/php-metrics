48360050f6f1c2a5a6451a4ef05492817f08ca26
nikulin <nikulin@tutu.ru>
2019-08-06 10:27:42 +0300

    fix format

--- src/StatsdExporterClient.php
+++ src/StatsdExporterClient.php
@@ -24,6 +24,7 @@ class StatsdExporterClient implements StatsdExporterClientInterface
     /** @var Client */
     private $statsdClient;
 
+
     public function __construct(string $appName, string $host, int $port, float $timeoutSec)
     {
         $this->appName = $appName;
@@ -32,48 +33,49 @@ class StatsdExporterClient implements StatsdExporterClientInterface
         $this->timeoutSec = $timeoutSec;
     }
 
+
     public function count(string $key, float $value, array $tags = []): StatsdExporterClientInterface
     {
         $this->statsdClient()->count($this->prepareKey($key), $value, $sampleRate = 1, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function increment(string $key, array $tags = []): StatsdExporterClientInterface
     {
         $this->statsdClient()->increment($this->prepareKey($key), $sampleRate = 1, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function decrement(string $key, array $tags = []): StatsdExporterClientInterface
     {
         $this->statsdClient()->decrement($this->prepareKey($key), $sampleRate = 1, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function timing(string $key, float $seconds, array $tags = []): StatsdExporterClientInterface
     {
         $ms = (float)($seconds * 1000);
         $this->statsdClient()->timing($this->prepareKey($key), $ms, $sampleRate = 1, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function gauge(string $key, float $value, array $tags = []): StatsdExporterClientInterface
     {
         $this->statsdClient()->gauge($this->prepareKey($key), $value, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function summary(string $key, float $value, array $tags = []): StatsdExporterClientInterface
     {
         $this->statsdClient()->timing($this->prepareKey($key), $value, $sampleRate = 1, $this->prepareTags($tags));
-
         return $this;
     }
+
 
     public function save(): void
     {
@@ -81,10 +83,12 @@ class StatsdExporterClient implements StatsdExporterClientInterface
         $this->resetClient();
     }
 
+
     protected function createStatsdConnection(): Connection
     {
         return new UdpSocket($this->host, $this->port, $this->timeoutSec, true);
     }
+
 
     private function statsdClient(): Client
     {
@@ -93,19 +97,21 @@ class StatsdExporterClient implements StatsdExporterClientInterface
             $this->statsdClient = new Client($connection);
             $this->statsdClient->startBatch();
         }
-
         return $this->statsdClient;
     }
+
 
     private function resetClient(): void
     {
         $this->statsdClient = null;
     }
 
+
     private function prepareKey(string $key): string
     {
         return preg_replace('/[^a-zA-Z0-9_]+/', '_', $key);
     }
+
 
     private function prepareTags(array $tags): array
     {
@@ -113,7 +119,6 @@ class StatsdExporterClient implements StatsdExporterClientInterface
         foreach ($tags as $k => $v) {
             $preparedTags[$this->prepareKey($k)] = $v;
         }
-
         return $preparedTags;
     }
 }
